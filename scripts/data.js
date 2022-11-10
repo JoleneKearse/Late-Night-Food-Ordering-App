5bf749889efe25f69a18343378aa507f66cb4f15
Jolene Kearse <pharaohnutz@gmail.com>
2022-11-10 11:14:06 -0500

    chore: initial setup

---
+++ scripts/data.js
@@ -0,0 +1,54 @@
+const menuArr = [
+  {
+    name: "Dumplings",
+    id: 0,
+    description:
+      "6 dumplings filled with ground pork and your choice of cabbage or corn",
+    price: 8,
+    img: "https://img.icons8.com/external-icongeek26-flat-icongeek26/64/null/external-dumplings-fine-dining-icongeek26-flat-icongeek26.png",
+  },
+  {
+    name: "Fried Noodles",
+    id: 1,
+    description:
+      "Filling fried, greasy dish full of veggies, chicken and shrimp",
+    price: 10,
+    img: "https://img.icons8.com/external-photo3ideastudio-lineal-color-photo3ideastudio/64/null/external-noodles-china-photo3ideastudio-lineal-color-photo3ideastudio.png",
+  },
+  {
+    name: "BBQ Skewers",
+    id: 2,
+    description:
+      "Load up with a variety of pork, chicken, tofu and veggie skewers",
+    price: 3,
+    img: "https://img.icons8.com/external-icongeek26-flat-icongeek26/64/null/external-dumplings-fine-dining-icongeek26-flat-icongeek26.png",
+  },
+  {
+    name: "Ramen",
+    id: 3,
+    description: "Warming soup with veggies, noodles, and pork",
+    price: 8,
+    img: "https://img.icons8.com/emoji/96/null/steaming-bowl-emoji.png",
+  },
+  {
+    name: "Baozi",
+    id: 4,
+    description: "Filling buns full of cabbage and pork or chicken",
+    price: 4,
+    img: "https://img.icons8.com/emoji/96/null/steaming-bowl-emoji.png",
+  },
+  {
+    name: "Congee",
+    id: 5,
+    description: "THE Chinese soup of rice, veggies and shrimp",
+    price: 4,
+    img: "https://img.icons8.com/emoji/96/null/steaming-bowl-emoji.png",
+  },
+  {
+    name: "Beer",
+    id: 6,
+    description: "Big bottle to wash everything down",
+    price: 2.5,
+    img: "https://img.icons8.com/office/80/null/beer-bottle.png",
+  },
+];
